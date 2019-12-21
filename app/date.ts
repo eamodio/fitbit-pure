1dc4ac0611e52e0466dfee0a031999e433103737
Eric Amodio <eamodio@gmail.com>
2019-12-21 14:36:59 -0500

    Fixes date highlight with different locales

--- app/date.ts
+++ app/date.ts
@@ -1,7 +1,8 @@
 import clock, { TickEvent } from 'clock';
 import { display, Display } from 'display';
+import { gettext } from 'i18n';
+import { locale } from 'user-settings';
 import { ConfigChanged, configuration } from './configuration';
-import { getLocalizedDate } from './locale';
 import { addEventListener, defer, Disposable, log } from '../common/system';
 
 export class DateDisplay {
@@ -66,9 +67,55 @@ export class DateDisplay {
 	render() {
 		const date = this._date ?? new Date();
 
-		this.$date.text = getLocalizedDate(date);
+		const month = date.getMonth();
+		const monthName = gettext(`month_short_${month}`);
 
-		const x = this.$date.getBBox().right;
+		const day = date.getDay();
+		const dayName = gettext(`day_short_${day}`);
+
+		const dayMonthSeparator = gettext('day_month_separator');
+
+		let x: number;
+		switch (locale.language) {
+			case 'zh-cn':
+				// 2月7日周二 = Month_Date_Weekday
+				this.$date.text = `${monthName}${dayMonthSeparator}${date.getDate()}`;
+				x = this.$date.getBBox().width;
+				this.$date.text += `${dayName}`;
+				x += this.$date.getBBox().left;
+				break;
+			case 'ja-jp':
+				// 8月3日（木）= Month_Date (Weekday)
+				this.$date.text = `${monthName}${dayMonthSeparator}${date.getDate()}`;
+				x = this.$date.getBBox().width;
+				this.$date.text += ` (${dayName})`;
+				x += this.$date.getBBox().left;
+				break;
+			case 'ko-kr':
+				// 2/7 (목) = Month/date (day)
+				this.$date.text = `${date.getMonth() + 1}${dayMonthSeparator}${date.getDate()}`;
+				x = this.$date.getBBox().width;
+				this.$date.text += ` (${dayName})`;
+				x += this.$date.getBBox().left;
+				break;
+			case 'en-us':
+			case 'en-ca':
+			case 'es-pa':
+			case 'es-pr':
+			case 'en-se':
+				// Thu, Feb 7
+				this.$date.text = `${dayName}${dayMonthSeparator} ${monthName} ${date.getDate()}`;
+				x = this.$date.getBBox().right;
+				break;
+			default:
+				// Thu, 7 Feb
+				this.$date.text = `${dayName}${dayMonthSeparator} ${date.getDate()}`;
+				x = this.$date.getBBox().width;
+				this.$date.text += ` ${monthName}`;
+				x += this.$date.getBBox().left;
+				break;
+		}
+
 		this.$dateHighlight.x = x;
 		this.$dateHighlight.text = `${date.getDate()}`;
 	}
