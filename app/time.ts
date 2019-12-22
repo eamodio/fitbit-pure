30876d0b8a194405f12c9a65be7cac797ee3082e
Eric Amodio <eamodio@gmail.com>
2019-12-22 00:05:30 -0500

    Fixes issue with leading 0 on tick Changes text/icon opacity to be 100% by default

--- app/time.ts
+++ app/time.ts
@@ -5,6 +5,8 @@ import { ConfigChanged, configuration } from './configuration';
 import { defer, log } from '../common/system';
 
 const emptyDate = new Date(0, 0, 0, 0, 0, 0, 0);
+const dimClass = /\bdim\b/;
+const dimReplacementClass = /\bdim\b/;
 
 export class TimeDisplay {
 	private _date: Date | undefined;
@@ -76,15 +78,21 @@ export class TimeDisplay {
 		this.$hour0.href = `images/${hour[0] ?? 0}.png`;
 		if (hour[0] === '0') {
 			if (configuration.get('showLeadingZero')) {
-				this.$hour0.style.visibility = 'visible';
-				this.$hour0.style.fillOpacity = 0.4;
+				if (!dimClass.test(this.$hour0.class)) {
+					this.$hour0.class += ' dim';
+				}
+
 				this.$container.groupTransform!.translate.x = 0;
+				this.$hour0.style.visibility = 'visible';
 			} else {
 				this.$hour0.style.visibility = 'hidden';
 				this.$container.groupTransform!.translate.x = -33;
 			}
 		} else {
-			this.$hour0.style.fillOpacity = 0.7;
+			this.$hour0.class = this.$hour0.class.replace(dimReplacementClass, '');
+
+			this.$container.groupTransform!.translate.x = 0;
+			this.$hour0.style.visibility = 'visible';
 		}
 		this.$hour1.href = `images/${hour[1] ?? 0}.png`;
 
